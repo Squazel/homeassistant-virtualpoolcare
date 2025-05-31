e72b1b5ba6ef156cee6662135e2b84d50b9771bb
Michael Downward <michael@aapl.net.au>
2025-05-31 16:31:54 +1000

    * Further fixes to github workflows

--- custom_components/virtualpoolcare/__init__.py
+++ custom_components/virtualpoolcare/__init__.py
@@ -3,10 +3,16 @@
 # Handle case where homeassistant module might not be available (during testing)
 try:
     from homeassistant.core import HomeAssistant
+    from homeassistant.helpers import config_validation as cv
     from .const import DOMAIN
+    
+    # Define config schema - this integration uses config entry only
+    CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+    
 except ImportError:
     # This allows the module to be imported during testing without Home Assistant
     HomeAssistant = None
+    CONFIG_SCHEMA = None
     from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
